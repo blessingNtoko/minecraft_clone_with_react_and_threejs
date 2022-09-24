750dcce465252b8eb22da68357b435ffd10cedad
Blessing <bntoko@gmail.com>
2022-09-24 19:42:20 +0200

    added plane geometry for ground; added physics jsx element; created and added player jsx element; created and added ground jsx element;

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Canvas } from "@react-three/fiber";
+import { Sky } from '@react-three/drei';
+import { Physics } from "@react-three/cannon";
+import { Ground } from "./components/ground";
+import { Player } from "./components/player";
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
-      <Canvas></Canvas>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]}/>
+        <ambientLight intensity={.5}/>
+        <Physics>
+          <Player />
+          <Ground />
+        </Physics>
+      </Canvas>
     </>
   );
 }
