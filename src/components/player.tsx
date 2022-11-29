1e35a70cc6411524432fd0855b38b7cb0e11261a
Blessing <bntoko@gmail.com>
2022-11-29 23:19:35 +0200

    added movement to player

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,44 +1,62 @@
-import { useFrame, useThree } from '@react-three/fiber';
-import { useSphere } from '@react-three/cannon';
-import { useEffect, useRef } from 'react';
-import { Vector3 } from 'three';
-import { useKeyboard } from '../hooks/useKeyboard';
+import { useFrame, useThree } from "@react-three/fiber";
+import { useSphere } from "@react-three/cannon";
+import { useEffect, useRef } from "react";
+import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
 
-const JUMP_FORCE = 5;
-const FORWARD_FORCE = 3;
+const JUMP_FORCE = 4;
+const SPEED = 4;
 
 export const Player = () => {
-    const actions = useKeyboard();
-    console.log('@Player | actions :: ', Object.entries(actions).filter(([k, v]) => v));
-    
+  const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+    useKeyboard();
 
-    const { camera } = useThree();
-    const [ref, api]: any = useSphere(() => ({
-        mass: 1,
-        type: 'Dynamic',
-        position: [0, 1, 0]
-    }));
+  const { camera } = useThree();
+  const [ref, api]: any = useSphere(() => ({
+    mass: 1,
+    type: "Dynamic",
+    position: [0, 1, 0],
+  }));
 
-    const vel = useRef([0, 0, 0]);
-    useEffect(() => {
-        api.velocity.subscribe((v: any) => vel.current = v);
-    }, [api.velocity]);
+  const vel = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.velocity.subscribe((v: any) => (vel.current = v));
+  }, [api.velocity]);
 
-    const pos = useRef([0, 0, 0]);
-    useEffect(() => {
-        api.position.subscribe((p: any) => pos.current = p);
-    }, [api.position]);
+  const pos = useRef([0, 0, 0]);
+  useEffect(() => {
+    api.position.subscribe((p: any) => (pos.current = p));
+  }, [api.position]);
 
-    useFrame(() => {
-        camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
-
-        if (actions.jump && Math.abs(vel.current[1]) < 0.05) {
-            api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
-        }
-    });
-
-    return (
-        <mesh ref={ref}></mesh>
+  useFrame(() => {
+    camera.position.copy(
+      new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
 
-}
+    const direction = new Vector3();
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector, sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation);
+
+    api.velocity.set(direction.x, vel.current[1], direction.z);
+
+    if (jump && Math.abs(vel.current[1]) < 0.05) {
+      api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+    }
+  });
+
+  return <mesh ref={ref}></mesh>;
+};
