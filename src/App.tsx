ce2c78e262e1535e7b6b8afd60025943c4a3e8a9
Blessing <bntoko@gmail.com>
2022-11-29 23:25:58 +0200

    locked camera to mouse position

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei';
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./components/ground";
 import { Player } from "./components/player";
+import { FPV } from "./components/fpv";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]}/>
         <ambientLight intensity={.5}/>
+        <FPV />
         <Physics>
           <Player />
           <Ground />
